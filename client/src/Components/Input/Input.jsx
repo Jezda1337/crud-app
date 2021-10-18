f39b11dd140c8bafee58a9e5710b0a32e59e4ee1
Jezda1337 <megasrbin1@gmail.com>
2021-10-18 20:32:53 +0200

    add tailwind to react

---
+++ client/src/Components/Input/Input.jsx
@@ -0,0 +1,11 @@
+const Input = () => {
+
+  return (
+    <>
+      <label className="label__wrapp">
+        <input className="" type={type} placeholder={placeholder} onChange={handleChange} />
+      </label>
+    </>
+  )
+
+}
